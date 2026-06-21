1a401853c41a72fdcb70c9ca1c2b17eb104ef72d
cheng362 <2680800294@qq.com>
2026-06-21 16:48:13 +0800

    feat: implement authentication and Prisma integration with new route and client files

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,19 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-	appname: process.env.APP_NAME,
-	baseURL: process.env.BETTER_AUTH_URL,
-	emailAndPassword: {
-		enabled: true,
-	},
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
 });
---
+++ src/lib/prisma.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+import { PrismaClient } from "../../generated/prisma/client";
+
+const adapter = new PrismaMariaDb(
+  {
+    host: process.env.MYSQL_HOST!,
+    user: process.env.MYSQL_USER!,
+    password: process.env.MYSQL_PASSWORD!,
+    connectionLimit: 5,
+  },
+  {
+    database: process.env.MYSQL_NAME!,
+  }
+);
+
+export const prisma = new PrismaClient({
+  adapter,
+});
